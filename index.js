af49cc4343443f3661ba5847b220e5f4919b26a8
gideonekekeke <Gideonekeke64@gmail.com>
2023-02-11 22:33:32 +0100

    commited

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const url = "mongodb://localhost/emailTestingDB";
+const lifeURI =
+	"mongodb+srv://uniabuja-server:uniabuja-server@cluster0.8usbdhw.mongodb.net/server?retryWrites=true&w=majority";
+
 const router = require("./Routes/EmailRoutes");
 const imagerouter = require("./Routes/Imagerouter");
 
 const port = 3030;
 
 const app = express();
-mongoose.connect(url).then(() => {
+mongoose.connect(lifeURI).then(() => {
 	console.log("connected");
 });
 
